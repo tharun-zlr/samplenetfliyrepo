ef3f4e9e0ed481a34ae3891db8e8639cae139778
tharun-zlr <you@example.com>
2023-08-18 16:18:07 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <h1>HELLO WELCOME TO THE ZELARSOFT</h1>
+      </center>
+    </div>
+  )
+}
+
+export default App
